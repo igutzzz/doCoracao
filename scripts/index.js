8472fa4bf589516be9917736cd2b061b6f78ac90
Igor de Almeida <ofc.iguituu@gmail.com>
2022-05-08 21:52:16 -0300

    Ajustes da página inicial

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+let images = ["./img/escola.png", "./img/animais.png", "./img/floresta.png"]
+
+let image = document.getElementById('image_hero');
+
+console.log(image)
+
+function ImageChanger(){
+
+}
+
+ImageChanger()
