9c601f27f9e0d3dff988443dbdea0ce7653b4cfb
igutzzz <ofc.iguituu@gmail.com>
2022-05-09 23:56:54 -0300

    Página Inicial pronta

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
-let images = ["./img/escola.png", "./img/animais.png", "./img/floresta.png"]
+let menu = document.getElementById('hamburger_menu');
+let menuItems = document.getElementById('navbar_menu');
 
-let image = document.getElementById('image_hero');
-
-console.log(image)
-
-function ImageChanger(){
-
+const toggleMenu = () => {
+    menuItems.classList.toggle('open');
+    if(menu.textContent == 'menu'){
+        menu.textContent = 'close';
+    } else {
+        menu.textContent = 'menu';
+    }
+    console.log(menu.textContent);
 }
 
-ImageChanger()
+menu.addEventListener('click', toggleMenu);
